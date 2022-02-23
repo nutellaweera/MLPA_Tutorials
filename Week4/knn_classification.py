e04b4864f284efecb0a546232437cb6ab17d5453
Sunella <8774488+nutellaweera@users.noreply.github.com>
2022-02-23 15:11:05 +0000

    Accuracy plot

--- Week4/knn_classification.py
+++ Week4/knn_classification.py
@@ -6,7 +6,7 @@ from sklearn.neighbors import KNeighborsClassifier
 import matplotlib.pyplot as plt
 
 df = pd.read_csv('Week4/breast_cancer_data.txt')
-print(df.head(5))
+#print(df.head(5))
 
 df.replace('?', -99999, inplace=True)
 df.drop(['id'], 1, inplace=True)
@@ -32,3 +32,22 @@ for i in range(1,20):
 # Test accuracy of training and testing for different Ks; most accurate when K=5
 # Evaluate with id included; test and training scores are worse because the parameter isn't correlated
 # Other parameters; default distance (metric) is Minkowski, changing to Euclidean or Manhattan affects K
+
+data = np.array([4,3,3,2,1,2,1,1,2])
+prediction = clf.predict(data.reshape(1, -1))
+print(prediction)
+
+train_acc, test_acc = [],[]
+
+for i in range(1,11):
+    clf = KNeighborsClassifier(i)
+    clf.fit(x_train, y_train)
+    train_acc.append(clf.score(x_train, y_train))
+    test_acc.append(clf.score(x_test, y_test))
+
+plt.plot(range(1,11), train_acc, label="training accuracy")
+plt.plot(range(1,11), test_acc, label="testing accuracy")
+plt.ylabel("Accuracy")
+plt.xlabel("N_Neighbors")
+plt.legend()
+plt.show()
