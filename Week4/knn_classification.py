20dbe6b69dc8200e664eeda44e974a554d4a726e
Sunella <8774488+nutellaweera@users.noreply.github.com>
2022-02-26 14:03:12 +0000

    Confusion matrix and plots.

--- Week4/knn_classification.py
+++ Week4/knn_classification.py
@@ -5,13 +5,18 @@ from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 import matplotlib.pyplot as plt
 from sklearn.datasets import load_breast_cancer
+from sklearn.metrics import plot_confusion_matrix
 
-def plot(train_scores, test_scores):
+def gen_plot(train_scores, test_scores):
     plt.plot(range(1,11), train_scores, label="training accuracy")
     plt.plot(range(1,11), test_scores, label="testing accuracy")
     plt.ylabel("Accuracy")
     plt.xlabel("N_Neighbors")
     plt.legend()
+    plt.show()
+
+def gen_confusion_matrix(clf, x_test, y_test):
+    plot_confusion_matrix(clf, x_test, y_test, cmap=plt.cm.Blues)
     plt.show()
 
 def load_from_txt():
@@ -55,25 +60,37 @@ def load_from_txt():
         train_acc.append(clf.score(x_train, y_train))
         test_acc.append(clf.score(x_test, y_test))
 
-    plot(train_acc, test_acc)
+    gen_plot(train_acc, test_acc)
     # Intepretation of plot; there are multiple high/low points (possible K), visually k=5 still seems like a good fit 
 
 
 def load_from_sklearn():
     cancer_ds = load_breast_cancer()
+    # print(type(cancer_ds)) sklearn bunch
+    df = pd.DataFrame(cancer_ds.data, columns=cancer_ds.feature_names)
+    df['target'] = cancer_ds.target
+    # print(df.info) 569 rows, 31 cols (inc target), no visible missing values
     # random state - shuffling with reproducibility, stratify - class labels
     x_train, x_test, y_train, y_test = train_test_split(cancer_ds.data, cancer_ds.target, stratify=cancer_ds.target, random_state=66)
     
     train, test = [], []
-    for i in range(1,12):
+    for i in range(1,11):
         clf = KNeighborsClassifier(n_neighbors=i)
         clf.fit(x_train, y_train)
         train.append(clf.score(x_train, y_train))
         test.append(clf.score(x_test, y_test))
     
-    plot(train, test)
+    gen_plot(train, test)
+    # best fit when k=6
+
+    clf = KNeighborsClassifier(6)
+    clf.fit(x_train, y_train)
+    gen_confusion_matrix(clf, x_test, y_test)
+    # TP 48 | FP 5
+    # FN 4  | TN 86
+    # high accuracy because true classifications are much larger values than FN and FP
 
 
 #load_from_txt()
-#load_from_sklearn()
+load_from_sklearn()
 
