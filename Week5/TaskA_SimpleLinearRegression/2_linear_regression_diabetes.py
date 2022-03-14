3757b9db18fbda8471df062c333413873112b9fe
Sunella <8774488+nutellaweera@users.noreply.github.com>
2022-03-14 23:22:13 +0000

    Restructuring

--- Week5/TaskA_SimpleLinearRegression/2_linear_regression_diabetes.py
+++ Week5/TaskA_SimpleLinearRegression/2_linear_regression_diabetes.py
@@ -33,7 +33,6 @@ print(f'Coefficient of determination r squared: {r2_score(y_test, y_pred)}')
 # low r^2 - independent variable does not explain variance of the dependent variable
 # could be a good model but likely not to be
 
-
 # plt.scatter(x_test, y_test, color='black')
 # plt.plot(x_test, y_pred, color='blue', linewidth=3)
 # plt.xticks()
---
+++ Week5/TaskB2_MultipleLinearRegression/4_ridge_and_residuals.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn import datasets
+from sklearn.preprocessing import MinMaxScaler, PolynomialFeatures
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import Ridge
+
+def load_ext_boston():
+    boston = datasets.load_boston()
+    x = boston.data
+
+    x = MinMaxScaler().fit_transform(boston.data)
+    x = PolynomialFeatures(degree=1, include_bias=False).fit_transform(x)
+    return x, boston.target
+
+x,y = load_ext_boston()
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=0)
+lr = LinearRegression().fit(x_train, y_train)
+
+ridge = Ridge(alpha=1.0).fit(x_train, y_train)
+
+print(f'Training set score: {lr.score(x_train, y_train)}') #0.77
+print(f'Test set score: {lr.score(x_test, y_test)}') #0.64
+print(f'Ridge training set score: {ridge.score(x_train, y_train)}') #0.77
+print(f'Ridge test set score: {ridge.score(x_test, y_test)}') #0.62
+
